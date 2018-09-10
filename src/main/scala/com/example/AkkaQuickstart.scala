80ae2e87400e8ac202418ef3175562cf364d7154
sweeneyb <tab126@gmail.com>
2018-09-09 20:43:44 -0500

    pure scala impl

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,56 +1,10 @@
-//#full-example
-package com.example
-
-import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
 import scala.collection.mutable.ListBuffer
 
-//#greeter-companion
-//#greeter-messages
-object Greeter {
-  //#greeter-messages
-  def props(message: String, printerActor: ActorRef): Props = Props(new Greeter(message, printerActor))
-  //#greeter-messages
-  final case class WhoToGreet(who: String)
-  case object Greet
-}
-//#greeter-messages
-//#greeter-companion
-
-//#greeter-actor
-class Greeter(message: String, printerActor: ActorRef) extends Actor {
-  import Greeter._
-  import Printer._
-
-  var greeting = ""
-
-  def receive = {
-    case WhoToGreet(who) =>
-      greeting = message + ", " + who
-    case Greet           =>
-      //#greeter-send-message
-      printerActor ! Greeting(greeting)
-      //#greeter-send-message
-  }
-}
-//#greeter-actor
-
-//#printer-companion
-//#printer-messages
-object Printer {
-  //#printer-messages
-  def props: Props = Props[Printer]
-  //#printer-messages
-  final case class Greeting(greeting: String)
-}
-//#printer-messages
-//#printer-companion
-
-//#printer-actor
-class Printer extends Actor with ActorLogging {
-  import Printer._
-
-  def receive = {
-    case Greeting(greeting) =>
+//#main-class
+object AkkaQuickstart extends App {
+//  println("Hello, World")
+ def receive = {
+    
     val bufferedSource = io.Source.fromFile("./src/main/resources/agnc-dividends.csv")
     var dividendList = new ListBuffer[Double]()
     // var dividendList = List(1.0, 2.0)
@@ -58,37 +12,15 @@ class Printer extends Actor with ActorLogging {
         val cols = line.split(",").map(_.trim)
         dividendList += cols(2).toDouble
         // do whatever you want with the columns here
-        println(s"${cols(2)}")
+        // println(s"${cols(2)}")
     }
     val foo = 
-    log.info("Total distributions for 1 share:  "+dividendList.foldLeft(0.0) (_ + _))
+    println("Total distributions for 1 share:  "+dividendList.foldLeft(0.0) (_ + _))
     bufferedSource.close
-    log.info("Greeting received (from " + sender() + "): " + greeting)
+    
   }
-}
-//#printer-actor
-
-//#main-class
-object AkkaQuickstart extends App {
-  import Greeter._
-
-  // Create the 'helloAkka' actor system
-  val system: ActorSystem = ActorSystem("helloAkka")
-
-  //#create-actors
-  // Create the printer actor
-  val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
-
-  // Create the 'greeter' actors
-  val howdyGreeter: ActorRef =
-    system.actorOf(Greeter.props("Howdy", printer), "howdyGreeter")
-  //#create-actors
-
   //#main-send-messages
-  howdyGreeter ! WhoToGreet("Akka")
-  howdyGreeter ! Greet
-
-  //#main-send-messages
+  receive
 }
 //#main-class
 //#full-example
