21f3d6f0b6b8b562fd2333d072d2a753901abb91
sweeneyb <tab126@gmail.com>
2018-09-09 21:45:25 -0500

    parses dividends and price quotes

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,26 +1,41 @@
 import scala.collection.mutable.ListBuffer
+import java.time.LocalDate
+import java.time.format._
 
-//#main-class
+
+case class Dividend( exDate: LocalDate, amount: Double)
+case class Quote( date: LocalDate, open: Double, high: Double, low: Double, close: Double, volume: Int)
+
 object AkkaQuickstart extends App {
 //  println("Hello, World")
- def receive = {
-    
-    val bufferedSource = io.Source.fromFile("./src/main/resources/agnc-dividends.csv")
-    var dividendList = new ListBuffer[Double]()
-    // var dividendList = List(1.0, 2.0)
+
+val dtf = DateTimeFormatter.ofPattern("M/d/yyyy")
+  def loadDividends(filename: String) : List[Dividend] = {
+    val bufferedSource = io.Source.fromFile(filename)
+    var dividendList = new ListBuffer[Dividend]()
     for (line <- bufferedSource.getLines) {
         val cols = line.split(",").map(_.trim)
-        dividendList += cols(2).toDouble
-        // do whatever you want with the columns here
-        // println(s"${cols(2)}")
+        val entry = Dividend(LocalDate.parse(cols(0), dtf), cols(2).toDouble )
+        dividendList += entry
     }
-    val foo = 
-    println("Total distributions for 1 share:  "+dividendList.foldLeft(0.0) (_ + _))
     bufferedSource.close
-    
+    return dividendList.toList
   }
-  //#main-send-messages
-  receive
+
+    def loadQuotes(filename: String) : List[Quote] = {
+    val bufferedSource = io.Source.fromFile(filename)
+    var dividendList = new ListBuffer[Quote]()
+    for (line <- bufferedSource.getLines) {
+        val cols = line.split(",").map(_.trim)
+        val entry = Quote(LocalDate.parse(cols(0), dtf), cols(1).toDouble, cols(2).toDouble, cols(3).toDouble, cols(3).toDouble, cols(5).toInt )
+        dividendList += entry
+    }
+    bufferedSource.close
+    return dividendList.toList
+  }
+  
+  val divList = loadDividends("./src/main/resources/agnc-dividends.csv")
+  val priceList = loadQuotes("./src/main/resources/agnc-prices.csv")
+  println(divList.reverse.head.exDate + " " + divList.reverse.head.amount)
+  println(priceList.reverse.head.date + " " + priceList.reverse.head.close)
 }
-//#main-class
-//#full-example
